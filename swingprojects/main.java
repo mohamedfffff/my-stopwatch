30d0f9ec766fc3054771ad95e177daa791eb4fcd
the thing <65288778+mohamedfffff@users.noreply.github.com>
2024-06-09 13:27:33 +0300

    Initial commit

---
+++ swingprojects/main.java
@@ -0,0 +1,18 @@
+/*    */ package swingprojects;
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ public class main
+/*    */ {
+/*    */   public static void main(String[] args) {
+/* 10 */     new stopWatch();
+/*    */   }
+/*    */ }
+
+
+/* Location:              F:\Work Maybe\My Fucking Projects\my-stopwatch\My Fucking StopWatch.jar!\swingprojects\main.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       1.1.3
+ */
